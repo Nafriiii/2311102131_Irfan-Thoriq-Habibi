658e8360d59d37c9df663553893ba636df40526c
nafriiii <irfanthoriq012@gmail.com>
2024-03-06 16:45:49 +0700

    guided1

---
+++ Pertemuan01/guided1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    char op;
+    float num1,num2;
+    // it allows user to enter operator i.e.+,-,*,/
+    cout << "enter operator (+,-,*,/): ";
+    cin >> op;
+    //it allows user to enter the operands
+    cout << "enter two operands: ";
+    cin >> num1>> num2;
+    //switch statemant begins 
+    switch (op){
+        //if user enters +
+        case '+':
+        cout << "result: "<< num1+num2;
+        break;
+        // if user enters -
+        case '-':
+        cout << "result: "<<num1-num2;
+        break;
+        //if user enters *
+        case '*':
+        cout << "result: "<<num1 *num2;
+        break;
+        //if user enters /
+        case '/':
+        if (num2 !=0){
+            cout <<"result: "<< fixed<<setprecision(2)<<num1/num2;
+        }else {
+            cout <<"error ! Devision by zero is not allowed.";
+        }
+        break;
+        //if the operator is other than +,-,*,/
+        //eror message will display
+        default:
+        cout << " Error! Operator is not correct";
+    }//switch statment ends
+    return 0;
+}
